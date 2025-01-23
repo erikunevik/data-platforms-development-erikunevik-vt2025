afb011ccf2afc8896e67fa408788547da143ecce
erikunevik <erikunevik@gmail.com>
2025-01-23 08:54:41 +0100

    20250122

--- exercises/2/src/os_data.py
+++ exercises/2/src/os_data.py
@@ -1,6 +1,11 @@
 
 import sys
 import pkg_resources
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
 
 def main():
     # Get the Python version
@@ -16,6 +21,31 @@ def main():
 # Ensure the main function runs when the script is executed directly
 if __name__ == "__main__":
     main()
+
+
+df = pd.read_csv("athlete_events.csv")
+
+test = df["Team"].value_counts().reset_index()
+test_top = test.head(10)
+
+
+fig, ax = plt.subplots(1, figsize=(12, 6))
+ax.bar(x = test_top["Team"], height = test_top["count"])
+ax.set_ylabel("Antal tävlande")
+ax.set_xlabel("Land")
+ax.set_title("Top 10 nationer för antal tävlande i OS historien")
+
+plt.savefig("top_10nationer.jpeg", format = "jpeg", dpi = 300)
+
+plt.show()
+
+print("Nu är ploten skapad")
+
+
+
+# classes = df["Team"].value_counts().reset_index()
+# classes
+
 
 
 
