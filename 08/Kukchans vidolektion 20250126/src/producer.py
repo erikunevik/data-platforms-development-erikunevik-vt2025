87341d276f939d5e459fd124d68fdb55bcd535d5
Erik Unevik <erikunevik@gmail.com>
2025-01-27 21:10:12 +0100

    20250127

--- 08/Kukchans vidolektion 20250126/src/producer.py
+++ 08/Kukchans vidolektion 20250126/src/producer.py
@@ -7,11 +7,11 @@ data_path = Path(__file__).parents[1] / "data"
 #print(data_path)
 
 with open(data_path / "jokes.json", "r") as file:
-        jokes = json.load(file)
+        jokes = json.load(file) # Converts the json file to python format
         
 # pprint(jokes)
 
-app = Application(broker_address="localhost:9092", consumer_group="text-splitter")
+app = Application(broker_address="localhost:9092", consumer_group="text-splitter") # Communicates with the broker, broker_address är servern. 
                   
 #print(app)
 
@@ -24,13 +24,13 @@ jokes_topic = app.topic(name = "jokes", value_serializer="json")
 def main():
     with app.get_producer() as producer:
             
-        # print(producer)
+        print(producer)
                 
         for joke in jokes:
             kafka_msg = jokes_topic.serialize(key=joke["joke_id"], value = joke)
             print(f" Produced message: key = {kafka_msg.key, kafka_msg.value}")
                         
-            producer.produce(topic="jokes", key=str(kafka_msg.key), value=kafka_msg.value) 
+            producer.produce(topic="jokes", key=str(kafka_msg.key), value=kafka_msg.value) # Sends serialized message to kafka topic jokes
 # Run this script only when executing this script and when importing this module
 if __name__ == '__main__':
         main()
--- Code_alongs/08/src/producer.py
+++ Code_alongs/08/src/producer.py
@@ -8,7 +8,7 @@ data_path = Path(__file__).parents[1] / "data"
 # print(data_path)
 
 with open(data_path / "jokes.json", "r") as file:
-    jokes = json.load(file)
+    jokes = json.load(file) 
 
 
 # pprint(jokes)
