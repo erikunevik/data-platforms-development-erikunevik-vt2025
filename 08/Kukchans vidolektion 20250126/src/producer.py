7d3e30d428719681774f5a3b2af5e736a82a23c5
Erik Unevik <erikunevik@gmail.com>
2025-01-27 13:50:15 +0100

    20250127

---
+++ 08/Kukchans vidolektion 20250126/src/producer.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import json
+from pprint import pprint
+from quixstreams import Application
+
+data_path = Path(__file__).parents[1] / "data"
+#print(data_path)
+
+with open(data_path / "jokes.json", "r") as file:
+        jokes = json.load(file)
+        
+# pprint(jokes)
+
+app = Application(broker_address="localhost:9092", consumer_group="text-splitter")
+                  
+#print(app)
+
+jokes_topic = app.topic(name = "jokes", value_serializer="json")
+
+#print(jokes_topic)
+
+# skapa kod
+
+def main():
+    with app.get_producer() as producer:
+            
+        # print(producer)
+                
+        for joke in jokes:
+            kafka_msg = jokes_topic.serialize(key=joke["joke_id"], value = joke)
+            print(f" Produced message: key = {kafka_msg.key, kafka_msg.value}")
+                        
+            producer.produce(topic="jokes", key=str(kafka_msg.key), value=kafka_msg.value) 
+# Run this script only when executing this script and when importing this module
+if __name__ == '__main__':
+        main()
